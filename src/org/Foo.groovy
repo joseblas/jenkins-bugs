8c2f036df31e82c6e4e43396155e54b53130e1bb
Jose Blas Camacho Taboada <joseblas@gmail.com>
2018-11-07 16:19:13 +0100

    Playing woth shared vars

--- src/org/Foo.groovy
+++ src/org/Foo.groovy
@@ -10,4 +10,15 @@ def checkOutFrom(repo) {
   git url: "git@github.com:jenkinsci/${repo}"
 }
 
+
+@Grab('org.apache.commons:commons-math3:3.4.1')
+import org.apache.commons.math3.primes.Primes
+
+void parallelize(int count) {
+  if (!Primes.isPrime(count)) {
+    error "${count} was not prime"
+  }
+  // …
+}
+
 return this
