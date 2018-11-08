b72cff40af4ce6e16421dff326e9ab55891f5283
Jose Blas Camacho Taboada <joseblas@gmail.com>
2018-11-08 10:27:19 +0100

    Playing woth shared vars

--- src/org/Foo.groovy
+++ src/org/Foo.groovy
@@ -16,7 +16,7 @@ def checkOutFrom(script, repo) {
 import org.apache.commons.math3.primes.Primes
 
 void parallelize(int count) {
-	// sayHello(count)
+	sayHello(count)
   if (!Primes.isPrime(count)) {
     error "${count} was not prime"
   }
